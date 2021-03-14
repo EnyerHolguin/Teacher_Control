fa1cbe39af103f034113a10fe3dfe865d6579a5a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-14 11:13:33 -0400

    Se creo la clase Asignaturas.

---
+++ Teacher_Control/Models/Asignaturas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teacher_Control.Models
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int AsignaturaId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
