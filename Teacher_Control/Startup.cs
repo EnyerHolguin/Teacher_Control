7dd8ef9d1c5b010464e99ca1d206d53553971daf
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-03-24 18:40:58 -0400

    algunos arreglo

--- Teacher_Control/Startup.cs
+++ Teacher_Control/Startup.cs
@@ -37,7 +37,7 @@ namespace Teacher_Control
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddRazorPages();
