41df91efcdbb063acc55fbf34db3913fc590ca1a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-03-28 09:02:45 -0400

    Se cambio el idioma del login.

---
+++ Teacher_Control/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Teacher_Control.Data;
+
+[assembly: HostingStartup(typeof(Teacher_Control.Areas.Identity.IdentityHostingStartup))]
+namespace Teacher_Control.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
